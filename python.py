72e2da1a05e9d1d3885a9f8d26b5eb554823b7f3
Said Ahmed <saidahmed9246@gemail.com>
2026-09-15 15:49:45 +0500

    update the print statement to correct the spelling of "word" to "world" and ensure proper spacing in the output.

--- python.py
+++ python.py
@@ -1,4 +1,22 @@
 
-print("hello word" , end="          py")
+print("hello word" , end="      ")
 
 print("hello word")
+
+# 1-data types 
+# int
+# float
+# str
+# bool
+
+age=40
+print(type(age))
+
+height=5.9
+print(type(height))
+
+name="Alice"
+print(type(name))
+
+is_student=True
+print(type(is_student))
