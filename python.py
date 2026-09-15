1bd7b8ab101e3d6c1285ae4821076cd93c2db0ea
Said Ahmed <saidahmed9246@gemail.com>
2026-09-15 16:35:33 +0500

    UPDATE

--- python.py
+++ python.py
@@ -1,23 +1,49 @@
 
-print("hello word" , end="      ")
+# print("hello word" , end="      ")
 
-print("hello word")
+# print("hello word")
 
-# 1-data types 
-# int
-# float
-# str
-# bool
+# # 1-data types 
+# # int
+# # float
+# # str
+# # bool
 
-age=40
-print(type(age))
+# age=40
+# print(type(age))
 
-height=5.9
-print(type(height))
+# height=5.9
+# print(type(height))
 
-name="Alice"
-print(type(name))
+# name="Alice"
+# print(type(name))
 
-is_student=True
-print(type(is_student))
+# is_student=True
+# print(type(is_student))
 
+# numbers = {10, 20, 20, 30} 
+# print(numbers)  
+
+# number1 =[10, 20, 20, 30]
+# print(number1)
+
+# students ={
+#     "name": "Alice",
+#     "age": 20,
+#     "is_student": True
+# }
+
+# print(students["name"])
+
+
+# for i in range(5):
+#     print(i)
+
+
+#     x= 10
+#     while x>0:
+#         print(x)
+#         x -= 1  
+
+while True:
+    print("Hello")
