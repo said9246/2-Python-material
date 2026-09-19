934032500e820172a44a7795af71613053548107
Said Ahmed <saidahmed9246@gmail.com>
2026-09-19 11:00:36 +0500

    add python operators file

---
+++ Lesson-1/1-python(operators).py
@@ -0,0 +1,54 @@
+a = 10
+b = 23
+print(a + b) # addition
+print(a - b) # Subtraction
+print(a * b) # Multiplication
+print(a / b) # Division
+print(a % b) # Remainder Operator(Modulus)
+print(a // b) # Integer Division(Gives answer in Integer)
+
+# Comparison Operators
+# == (Equal)
+print(a == b) # (False)
+
+# != (Not Equal)
+print(a != b) # (True)
+
+# > (Greater than)
+print(a > b) # (False)
+
+# < (Less than)
+print(a < b) # (True)
+
+# >= (Greater than or equal to)
+print(a >= b) # (False)
+
+# <= (Less than or equal to)
+print(a <= b) # (True) 
+
+
+# Logical Operators
+# and
+# or
+# not
+
+# And Operator
+print(a > 5 and b > 20) # (True) if both are true.
+print(a > 5 and b < 20) # (False) if anyone is false.
+
+# OR Operator
+print(a > 5 or b < 20) # (True) if anyone is true.
+print(a < 5 or b < 20) # (False) if both are true.
+
+# Not Operator
+print(not b < 20) # Reverse the result.
+
+
+# Concatenation Operator
+name1 = 'Said'
+name2 = 'Khan'
+print(name1 + name2)
+
+# Exponent Operator
+x = 2
+print(x**3)
