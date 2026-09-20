c20b646e04e8317cc3939eaa9fa1c091c7c6fb57
Said Ahmed <saidahmed9246@gmail.com>
2026-09-20 09:30:53 +0500

    updated code

--- corvit.py
+++ corvit.py
@@ -1,3 +1,10 @@
-import keyword
-print(keyword.kwlist)
-print(len(keyword.kwlist))
+# print('A'=="A")
+# print('A'==65)
+# print(ord('A')==65)
+
+
+x=2345
+y=2345
+
+print('id of x:',id(x))
+print('id of y:',id(y))
